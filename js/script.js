5729a2a49613c01f24e2d5bb0f5410c6d14dcb36
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-09-23 09:17:21 +0000

    Added isPrime function at script.js

--- js/script.js
+++ js/script.js
@@ -18,3 +18,14 @@ function getNumberInfo() {
     // Display Information in the Browser
     document.getElementById("numinfo").innerHTML = txt;
 }
+
+// Function to check if number num is prime
+function isPrime(num) {
+    if (PRIMES[num] !== undefined) return PRIMES[num];
+    if (num%2==0) return false;
+    const upperBound = Math.sqrt(num);
+    for (let i=3;i<=upperBound;i+=2) {
+        if (num%i===0) return PRIMES[num] = false;
+    }
+    return PRIMES[num] = true;
+}
