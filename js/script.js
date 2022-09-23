ac379954be7c1db9ce8e83faf31647508b7397d5
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-09-23 10:44:25 +0000

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Set Up Global Array
-const PRIMES = [false,false,true];
+const PRIMES = [false, false, true];
 
 // Function to Get Number Information (including Invalid Input)
 function getNumberInfo() {
@@ -8,7 +8,7 @@ function getNumberInfo() {
     // Get the value of the Input Field
     let num = document.getElementById("mynumber").value;
     // Check if the input is valid
-    if (isNaN(num) || num.length == 0 || num<4 || num>7 || (num.length > 1 && num[0] == "0") || !Number.isInteger(Number(num))) {
+    if (isNaN(num) || num.length == 0 || num < 4 || num > 7 || (num.length > 1 && num[0] == "0") || !Number.isInteger(Number(num))) {
         txt += `Invalid Input.  Please enter a whole number between 4 and 7.  Do not include leading zeros.`;
     } else {
         txt += `You have requested ${num} digits. <p>`;
@@ -22,10 +22,10 @@ function getNumberInfo() {
 // Function to check if number num is prime
 function isPrime(num) {
     if (PRIMES[num] !== undefined) return PRIMES[num];
-    if (num%2==0) return false;
+    if (num % 2 == 0) return false;
     const upperBound = Math.sqrt(num);
-    for (let i=3;i<=upperBound;i+=2) {
-        if (num%i===0) return PRIMES[num] = false;
+    for (let i = 3; i <= upperBound; i += 2) {
+        if (num % i === 0) return PRIMES[num] = false;
     }
     return PRIMES[num] = true;
 }
@@ -33,7 +33,7 @@ function isPrime(num) {
 // Function to check if number num is pandigital
 function isPandigital(num) {
     const numAsString = num.toString();
-    return Array(numAsString.length).fill(0).every((_,i) => numAsString.indexOf(i+1) !== -1);
+    return Array(numAsString.length).fill(0).every((_, i) => numAsString.indexOf(i + 1) !== -1);
 }
 
 /*
@@ -44,12 +44,12 @@ function isPandigital(num) {
     pandigitalPrime(7) returns 7652413
 */
 function pandigitalPrime(n) {
-    const upperBound = parseInt(Array(n).fill(0).map((_,i) => n-i).join(""));
-    for (let i=upperBound;i>0;i-=2) {
+    const upperBound = parseInt(Array(n).fill(0).map((_, i) => n - i).join(""));
+    for (let i = upperBound; i > 0; i -= 2) {
         if (isPandigital(i) && isPrime(i)) return i;
     }
 
-   
+
 }
 
 // Function to Clear Information
